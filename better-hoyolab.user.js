130bbc439ab6ce286c3b9345315f15f8f7ad6736
Aki <aki.oda.japan@gmail.com>
2024-01-16 13:50:50 -0500

    wildcard match

--- better-hoyolab.user.js
+++ better-hoyolab.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Better Hoyolab
 // @namespace    http://tampermonkey.net/
-// @version      2024-01-16.03
+// @version      2024-01-16.04
 // @description  try to take over the world!
 // @author       You
-// @match        https://act.hoyolab.com/app/community-game-records-sea/index.html
+// @match        https://act.hoyolab.com/app/community-game-records-sea/index.html*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=hoyolab.com
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
 // @grant        none
